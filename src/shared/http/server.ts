f18c94601d4a0e62bd2a7853e4f1a5afd11d7f39
rerbert brito viana <rerbertviana@gmail.com>
2021-07-28 23:35:48 -0300

    add typeorm and postgres

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,9 @@
-import express, { json } from 'express';
+import 'reflect-metadata';
+import express, { json, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
@@ -8,6 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+    (error: Error, request: Request, response: Response, next: NextFunction) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
 
 app.listen(3333, () => {
     console.log('Server started on port 3333!');
