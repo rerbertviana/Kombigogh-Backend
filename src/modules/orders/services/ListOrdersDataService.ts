c421e2a604b7e22c41a0676c139ad7a4f1f54c3f
rerbert brito viana <rerbertviana@gmail.com>
2021-10-19 03:54:12 -0300

    listorder por data e status

--- src/modules/orders/services/ListOrdersDataService.ts
+++ src/modules/orders/services/ListOrdersDataService.ts
@@ -1,6 +1,6 @@
 import { getMonth, getYear } from 'date-fns';
-import ListOrderService from './ListOrderService';
-
+import { getCustomRepository } from 'typeorm';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
 
 interface IRequest {
     mes: string;
@@ -11,17 +11,80 @@ class ListOrdersDataService {
     public async execute({ mes, ano }: IRequest): Promise<Object | undefined> {
         
         // listar todos os pedidos
-        const listorders = new ListOrderService();
-        const orders = await listorders.execute();
+        const ordersRepository = getCustomRepository(OrdersRepository);
+        const orders = ordersRepository.find();
+
+        const meses: any[] = [];
+
+        meses.push(
+            {
+                id: 0,
+                nome: 'JANEIRO'
+            },
+            {
+                id: 1,
+                nome: 'FEVEREIRO'
+            },
+            {
+                id: 2,
+                nome: 'MARÇO'
+            },
+            {
+                id: 3,
+                nome: 'ABRIL'
+            },
+            {
+                id: 4,
+                nome: 'MAIO'
+            },
+            {
+                id: 5,
+                nome: 'JUNHO'
+            },
+            {
+                id: 6,
+                nome: 'JULHO'
+            },
+            {
+                id: 7,
+                nome: 'AGOSTO'
+            },
+            {
+                id: 8,
+                nome: 'SETEMBRO'
+            },
+            {
+                id: 9,
+                nome: 'OUTUBRO'
+            },
+            {
+                id: 10,
+                nome: 'NOVEMBRO'
+            },
+            {
+                id: 11,
+                nome: 'DEZEMBRO'
+            },
+        );
 
         // converter os valores recebidos em inteiros
         const orderMes = parseInt(mes);
         const orderAno = parseInt(ano);
 
         // filtrar os pedidos pela datas
-        const ordersData = orders.filter(order => getMonth(order.created_at) === orderMes && getYear(order.created_at) === orderAno);
+        const ordersData = (await orders).filter(order => getMonth(order.created_at) === orderMes && getYear(order.created_at) === orderAno);
 
-        return ordersData;
+        //criar array com as informações que eu quero
+        const resul = ordersData.map(ord => ({
+            id: ord.id,
+            cliente: ord.cliente,
+            status: ord.status,
+            total: ord.total,
+            mes: meses.filter(p => p.id == getMonth(ord.created_at))[0].nome,
+            ano: getYear(ord.created_at)
+        }));
+
+        return resul;
     }
 }
 
--- src/modules/orders/services/ListOrdersStatusService.ts
+++ src/modules/orders/services/ListOrdersStatusService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from "typeorm";
 import OrdersRepository from "../typeorm/repositories/OrdersRepository";
+import { getMonth, getYear } from 'date-fns';
 
 interface Irequest {
     status: string;
@@ -14,11 +15,73 @@ class ListOrdersStatusService {
         const ordersRepository = getCustomRepository(OrdersRepository);
         const orders = ordersRepository.find();
 
+        //setar os meses
+        const meses: any[] = [];
+
+        meses.push(
+            {
+                id: 0,
+                nome: 'JANEIRO'
+            },
+            {
+                id: 1,
+                nome: 'FEVEREIRO'
+            },
+            {
+                id: 2,
+                nome: 'MARÇO'
+            },
+            {
+                id: 3,
+                nome: 'ABRIL'
+            },
+            {
+                id: 4,
+                nome: 'MAIO'
+            },
+            {
+                id: 5,
+                nome: 'JUNHO'
+            },
+            {
+                id: 6,
+                nome: 'JULHO'
+            },
+            {
+                id: 7,
+                nome: 'AGOSTO'
+            },
+            {
+                id: 8,
+                nome: 'SETEMBRO'
+            },
+            {
+                id: 9,
+                nome: 'OUTUBRO'
+            },
+            {
+                id: 10,
+                nome: 'NOVEMBRO'
+            },
+            {
+                id: 11,
+                nome: 'DEZEMBRO'
+            },
+        );
 
         // filtrar orders pelo status recebido
         const ordersStatus = (await orders).filter(order => order.status === status);
 
-        return ordersStatus;
+        const resul = ordersStatus.map(ord => ({
+            id: ord.id,
+            cliente: ord.cliente,
+            status: ord.status,
+            total: ord.total,
+            mes: meses.filter(p => p.id == getMonth(ord.created_at))[0].nome,
+            ano: getYear(ord.created_at)
+        }));
+
+        return resul;
     }
 }
 
